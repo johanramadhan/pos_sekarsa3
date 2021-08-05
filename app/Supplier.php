755753e4e6998da59c8d580852700226fcc92081
johanramadhan <71024772+johanramadhan@users.noreply.github.com>
2021-08-06 06:43:04 +0700

    add customer

---
+++ app/Supplier.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Supplier extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'suppliers';
+
+    protected $fillable = [
+        'name', 'slug', 'phone', 'address', 'email', 'description'
+    ];
+
+    protected $hidden = [
+        
+    ];
+}
