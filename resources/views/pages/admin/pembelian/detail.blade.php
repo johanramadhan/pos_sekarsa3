eb855eca03edfd5a98a5456811d5371d61fc378e
johanramadhan <71024772+johanramadhan@users.noreply.github.com>
2021-08-25 23:23:37 +0700

    oke transaction-detail

---
+++ resources/views/pages/admin/pembelian/detail.blade.php
@@ -0,0 +1,26 @@
+<div class="modal fade" id="modal-detail" tabindex="-1" role="dialog" aria-labelledby="modal-produk">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Detail Penjualan</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <table class="table table-striped table-bordered table-detail-penjualan">
+                <thead>
+                    <th width="5%" class="text-center">No</th>
+                    <th class="text-center">Kode Penjualan</th>
+                    <th class="text-center">Tanggal Transaksi</th>
+                    <th class="text-center">Nama Produk</th>
+                    <th class="text-center">Harga Jual</th>
+                    <th class="text-center">Jumlah</th>
+                    <th class="text-center">Diskon</th>
+                    <th class="text-center">Subtotal</th>
+                </thead>
+              </table>
+            </div>
+        </div>
+    </div>
+</div>
