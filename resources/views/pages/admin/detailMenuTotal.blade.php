141ef3c72ce957a3e80173243aaf38d74061c93c
johanramadhan <wanjohan5@gmail.com>
2021-11-15 14:36:25 +0700

    update dashboard admin

---
+++ resources/views/pages/admin/detailMenuTotal.blade.php
@@ -0,0 +1,34 @@
+<div class="modal fade" id="modal-menu-total" tabindex="-1" role="dialog" aria-labelledby="modal-persediaan">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Detail Menu Terjual</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <table class="table table-striped table-bordered table-detail-menu-total">
+                <thead>
+                  <tr>
+                    <th width="5%" class="text-center">No</th>
+                    <th class="text-center">Kode</th>
+                    <th class="text-center">Nama Menu</th>
+                    <th class="text-center">Jumlah Terjual</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($menu_terjual as $item)
+                     <tr>
+                      <td width="5%" class="text-center">{{ $loop->iteration }}</td>
+                      <td>{{ $item->produk->code }}</td>
+                      <td>{{ $item->produk->name_product }} - {{ $item->produk->diskon }}%</td>
+                      <td width="15%" class="text-center">{{ format_uang($item->total_jumlah) }}</td>
+                    </tr> 
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+        </div>
+    </div>
+</div>
